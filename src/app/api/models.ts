228d8ac2ed7b9ec37c9c31d7ec83328ad8f151ad
Adelane Junior <adelane.junior@aluno.ueg.br>
2023-08-24 19:36:38 -0300

    Adcionando arquitetura para login e mensagens, e configuração para pegar o codigo do back end

---
+++ src/app/api/models.ts
@@ -0,0 +1,6 @@
+export { FieldResponse } from './models/field-response';
+export { MessageResponse } from './models/message-response';
+export { UsuarioDto } from './models/usuario-dto';
+export { CredencialDto } from './models/credencial-dto';
+export { UsuarioSenhaDto } from './models/usuario-senha-dto';
+export { AuthDto } from './models/auth-dto';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,66 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from "@angular/material/form-field";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {LoaderModule} from "./arquitetura/loader/loader.module";
+import {LoaderDialogComponent} from "./arquitetura/loader-dialog/loader-dialog.component";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {AutenticacaoModule} from "./arquitetura/autenticacao/autenticacao.module";
+import {SecurityModule} from "./arquitetura/security/security.module";
+import {SecurityInterceptor} from "./arquitetura/security/security.interceptor";
+import {MessageModule} from "./arquitetura/message/message.module";
+import {AppInterceptor} from "./arquitetura/app.interceptor";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderDialogComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    LoaderModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    AutenticacaoModule,
+    MessageModule.forRoot(),
+    SecurityModule,//TODO conferir a configuração
+    SecurityModule.forRoot({
+      nameStorage: 'portalSSOSecurityStorage',
+      loginRouter: '/acesso/login'
+    }),
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}},
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AppInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SecurityInterceptor,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
