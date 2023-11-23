1e203eded3f6fcf929266541f02c558ef3113939
lucashspiess <lucashs725@gmail.com>
2023-11-23 13:51:36 -0300

    FIX checkbox de selecionar todos os produtos funcionando

--- src/app/pages/produto/list-produto/list-produto.component.ts
+++ src/app/pages/produto/list-produto/list-produto.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {MatDialog} from "@angular/material/dialog";
 import {MatSnackBar} from "@angular/material/snack-bar";
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 import {ProdutoControllerService} from "../../../api/services/produto-controller.service";
 import {ProdutoDto} from "../../../api/models/produto-dto";
 import {
@@ -11,6 +11,7 @@ import {
 } from "../../../core/confirmation-dialog/confirmation-dialog.component";
 import {EnvioMensagemComponent} from "../../../core/envio-mensagem/envio-mensagem.component";
 import {ImagemControllerService} from "../../../api/services/imagem-controller.service";
+
 @Component({
   selector: 'app-list-produto',
   templateUrl: './list-produto.component.html',
@@ -18,26 +19,43 @@ import {ImagemControllerService} from "../../../api/services/imagem-controller.s
 })
 
 
-export class ListProdutoComponent implements OnInit{
+export class ListProdutoComponent implements OnInit {
 
-  colunasMostrar = ['check', 'codigo','codigoBarras','imagem','categoriaId','nome', 'descricao','marca','quantidade','preco','acao'];
+  colunasMostrar = ['check', 'codigo', 'codigoBarras', 'imagem', 'categoriaId', 'nome', 'descricao', 'marca', 'quantidade', 'preco', 'acao'];
   produtoListaDataSource: MatTableDataSource<ProdutoDto> = new MatTableDataSource<ProdutoDto>();
   listProdutosEnviar: Array<ProdutoDto> = [];
-  linhaSelecionada!: ProdutoDto ;
+  listProdutosAux: Array<ProdutoDto> = [];
+  linhaSelecionada!: ProdutoDto;
+  allChecked: boolean = false;
+
   constructor(
     public produtoService: ProdutoControllerService,
     private dialog: MatDialog,
     private snackBar: MatSnackBar,
     private imagemService: ImagemControllerService,
     private router: Router
-  ){
+  ) {
   }
 
   ngOnInit(): void {
     this.buscarDados();
   }
 
-
+  selectAll(completed: boolean) {
+    this.listProdutosEnviar = [];
+    this.allChecked = completed;
+    if (this.allChecked) {
+      this.produtoService.produtoControllerListAll({}).subscribe(
+        retorno => {
+          this.listProdutosAux = retorno;
+          this.listProdutosAux.forEach(produto => {
+            this.selecionarLinha(produto);
+          })
+          console.log(this.listProdutosEnviar);
+        }
+      )
+    }
+  }
 
   private buscarDados() {
     this.produtoService.produtoControllerListAll().subscribe(data => {
@@ -53,12 +71,12 @@ export class ListProdutoComponent implements OnInit{
   remover(produtoDto: ProdutoDto) {
     console.log("Removido", produtoDto.codigo);
     let codigoDoProduto: number = produtoDto.codigo || 0;
-    this.produtoService.produtoControllerRemover({ id: codigoDoProduto})
+    this.produtoService.produtoControllerRemover({id: codigoDoProduto})
       .subscribe(
         retorno => {
           this.imagemService.imagemControllerExcluirFoto({id: retorno.imagemId}).subscribe();
           this.buscarDados();
-          if(retorno != null) {
+          if (retorno != null) {
             this.showMensagemSimples("Excluído com sucesso!", 5000);
             console.log("Exclusão:", retorno);
           }
@@ -87,7 +105,8 @@ export class ListProdutoComponent implements OnInit{
       }
     });
   }
-  showMensagemSimples( mensagem: string, duracao: number = 2000) {
+
+  showMensagemSimples(mensagem: string, duracao: number = 2000) {
     this.snackBar.open(mensagem, 'Fechar', {
       duration: duracao,
       horizontalPosition: 'center',
@@ -95,41 +114,40 @@ export class ListProdutoComponent implements OnInit{
     });
   }
 
-  openDialog(){
-    const dialogRef = this.dialog.open(EnvioMensagemComponent, )
-  }
-
-  enviarproduto(){
-      if(!this.listProdutosEnviar || this.listProdutosEnviar.length === 0){
-        this.showMensagemSimples("Selecione Produtos na checkBox da tabela para enviar!",5000)
-      }
-      else{
-      const enviarMensagem= this.dialog.open(EnvioMensagemComponent, {
+  enviarproduto() {
+    const dialogEnviarMensagem = () => {
+      return this.dialog.open(EnvioMensagemComponent, {
         data: {
           titulo: 'Enviar Produto Por E-mail',
           mensagem: `Selecione o cliente ou Digite o E-mail: `,
           dado: this.listProdutosEnviar
         },
       });
-      enviarMensagem.afterClosed().subscribe(() => {})
     }
-  }
-  selecionarLinha(row: ProdutoDto) {
-    // Verifica se a linha já está no array listProdutosEnviar
-    const index = this.listProdutosEnviar.indexOf(row);
-
-    if (index !== -1) {
-      // Se a linha já está no array, a remova
-      this.listProdutosEnviar.splice(index, 1);
+    if (!this.listProdutosEnviar || this.listProdutosEnviar.length === 0) {
+      this.showMensagemSimples("Selecione Produtos na checkBox da tabela para enviar!", 5000)
     } else {
-      // Se a linha não está no array, a adicione
-      this.listProdutosEnviar.push(row);
+      const enviarMensagem = dialogEnviarMensagem.call(this);
+      enviarMensagem.afterClosed().subscribe(() => {
+      })
     }
-
-    console.log(this.listProdutosEnviar);
   }
 
-  setAll(completed: boolean) {
+  selecionarLinha(produto: ProdutoDto) {
+    let removido = this.removeProdutoDaLista(produto);
+    if (!removido) {
+      this.listProdutosEnviar.push(produto);
+    }
   }
 
+  removeProdutoDaLista(produto: ProdutoDto) {
+    let removido = false;
+    this.listProdutosEnviar.forEach((item, index) => {
+      if (item.codigo === produto.codigo) {
+        this.listProdutosEnviar.splice(index, 1);
+        removido = true;
+      }
+    });
+    return removido;
+  }
 }
